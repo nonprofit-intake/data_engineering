3a5b5836cde56bb6a435e36ef996c64cc9e3c4bd
Serina Grill <gril0044@umn.edu>
2020-12-05 21:07:24 -0500

    renamed directories

---
+++ AWS_LAMBDA/remove_predictions_from_exited_guests/lambda_function.py
@@ -0,0 +1,71 @@
+import os
+import sys
+import logging
+import psycopg2
+
+# Tasks: 
+# 1. Connect to db
+# 2. Check if predicted_exit_destination column exists, and if not, add it
+# 3. Add values to predicted_exit_destination column
+
+# RDS settings
+rds_host = os.environ.get('RDS_HOST')
+rds_username = os.environ.get('RDS_USERNAME')
+rds_user_pwd = os.environ.get('RDS_USER_PWD')
+
+# Logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Connect to database
+try:
+    # If able to connect
+    conn = psycopg2.connect(
+        host=rds_host,
+        user=rds_username,
+        password=rds_user_pwd)
+except:
+    # If unable to connect
+    logger.error("ERROR: Could not connect to Postgres instance.")
+    sys.exit()
+
+# Log connection success
+logger.info("SUCCESS: Connection to RDS Postgres instance succeeded")
+
+
+def lambda_handler(event, context):
+    '''Add predicted_exit_destination column to guests_temp if not exists, 
+    then updates guests_temp to account for newly exited guests (if exited, no more need for prediction on row).'''
+
+    # Check if column exists
+    if conn != None: 
+        cur = conn.cursor()
+
+        try:
+            # If column exists
+            query = "SELECT predicted_exit_destination FROM guests_temp;"
+            cur.execute(query)
+            
+        except:
+            conn.rollback()
+            # If column doesn't exist
+            query = "ALTER TABLE guests_temp ADD predicted_exit_destination VARCHAR;"
+            cur.execute(query)
+
+    conn.commit()
+
+    # Check if column exists
+    with conn.cursor() as cur:
+
+        try:
+            # Update predictions
+            update_query = "UPDATE guests_temp SET predicted_exit_destination=CASE WHEN exit_date IS NOT NULL THEN 'exited' END;"
+            cur.execute(update_query)
+
+        except:
+            # Unable to update predictions table
+            logger.error("ERROR: Could not update predictions table.")
+            sys.exit()
+
+    conn.commit()
+    conn.close()
