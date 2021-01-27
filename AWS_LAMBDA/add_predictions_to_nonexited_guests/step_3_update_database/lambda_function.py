4758ea2c23dda15ccac096f39fbff99496105bd1
Serina Grill <gril0044@umn.edu>
2021-01-26 21:34:24 -0500

    updating readme to include non-technical documentation

--- AWS_LAMBDA/add_predictions_to_nonexited_guests/step_3_update_database/lambda_function.py
+++ AWS_LAMBDA/add_predictions_to_nonexited_guests/step_3_update_database/lambda_function.py
@@ -1,6 +1,3 @@
-# Downloads and opens csv of predictions from s3 bucket, 
-# uses these to update the database table with predictions.
-
 import pandas as pd
 import boto3
 import os
@@ -24,10 +21,21 @@ s3_bucket_origin = os.environ.get('S3_BUCKET_ORIGIN')
 predictions_file = os.environ.get('PREDICTIONS_FILE')
 predictions_file_path = '/tmp/' + predictions_file
 
+try:
+    conn = psycopg2.connect(
+        host=rds_host,
+        user=rds_username,
+        password=rds_user_pwd)
+except:
+    logger.error("ERROR: Could not connect to Postgres instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS Postgres instance succeeded")
+
 
 def lambda_handler(event, context):
     '''Downloads and opens csv of predictions from s3 bucket, uses these to update the 
-    database table with predictions.'''
+    database table guests with predictions where predictions is null.'''
 
     # download predictions csv from bucket
     s3.download_file(s3_bucket_origin, predictions_file, predictions_file_path)
@@ -36,25 +44,13 @@ def lambda_handler(event, context):
     with open(predictions_file_path, 'rb') as f:
         predictions = pd.read_csv(f) 
 
-    try:
-        conn = psycopg2.connect(
-            host=rds_host,
-            user=rds_username,
-            password=rds_user_pwd)
-    except:
-        logger.error("ERROR: Could not connect to Postgres instance.")
-        sys.exit()
-
-    logger.info("SUCCESS: Connection to RDS Postgres instance succeeded")
-
-
     # iterate through our results df
     for row in predictions.itertuples():
         with conn.cursor() as cur:
             # update database with our predictions
-            sql_query = 'UPDATE guests_temp SET predicted_exit_destination=%s WHERE personal_id=%s AND enroll_date=%s'
+            sql_query = 'UPDATE guests SET predicted_exit_destination=%s WHERE personal_id=%s AND enroll_date=%s'
             data = (row.predicted_exit_destination, row.personal_id, row.enroll_date)
             cur.execute(sql_query, data)
             conn.commit()
-    conn.close()
-    
+            
+    return 'status: 200'
